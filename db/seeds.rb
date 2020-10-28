5d36e0ccc3ba2a1648f82ddeb24934550c963b20
warrenniu <warrenniu@gmail.com>
2020-10-28 11:00:22 -0400

    Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+warren = Artist.create(name: "Warren", bio: "He is not very talented")
+conway = Artist.create(name: "Conway", bio: "He is not a very good dancer")
+
+rap = Genre.create(name: "Rap")
+blues = Genre.create(name: "Blues")
+
+money = Song.create(name: "Money", artist_id: 1, genre_id: 1)
+problems = Song.create(name: "Problems", artist_id: 2, genre_id: 2)
